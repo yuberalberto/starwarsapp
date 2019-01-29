c6551ce0549801c37995309a1badd57017570d7c
yuberalberto <yuberalberto@gmail.com>
2019-01-28 23:35:17 -0500

    added several cards and fixed some issues

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Card from './components/Card'
+import Card from '../components/Card' // import the component Card
 import 'tachyons'; // Styles, its something look alike bootstrap
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Card></Card>
+        <Card></Card>
         <Card></Card>
       </div>
     );
